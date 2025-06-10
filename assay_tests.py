0d5a749ea65523d5eb1175d2383f7e186427fa91
Tom Eulenfeld <tom.eulenfeld@uni-jena.de>
2025-06-10 15:32:16 +0200

    refactor test script

--- assay_tests.py
+++ assay_tests.py
@@ -2,17 +2,14 @@
 from contextlib import contextmanager
 import os
 from pathlib import Path
-import sys
 import tempfile
+import urllib.request
 
 
 @contextmanager
-def _tmpdir():
-    if '-h' in sys.argv:
-        print('Run tests for assay_blast and assay_analyze.\nOnly accepted argument is a tmpdir.')
-        sys.exit()
-    if len(sys.argv) == 2:
-        tmpdir = Path(sys.argv[1])
+def _tmpdir(out=None):
+    if out:
+        tmpdir = Path(out)
         tmpdir.mkdir(exist_ok=True)
         yield tmpdir
     else:
@@ -20,26 +17,48 @@ def _tmpdir():
             yield Path(tmpdir)
 
 
-def test_assay():
-    example = Path(__file__).parent
-    query = example / 'example_queries.fasta'
-    genomes = example / 'example_database.fasta'
-    with _tmpdir() as tmp:
-        out = tmp / 'probes.blastn'
-        db = tmp / 'db.db'
-        call = f'assay_blast.py {genomes} -q {query} -o {out} --db {db}'
-        print(call)
-        os.system(call)
-        os.system(call)
-        call = f'assay_analyze.py {out}'
-        print(call)
-        os.system(call)
-        call = f'assay_analyze.py {out} --only-primer -o {tmp / "primer"}'
-        print(call)
-        os.system(call)
-    print()
-    print('Tests run successful.')
+_URL = 'https://raw.githubusercontent.com/mcollatz/assayBLAST/refs/heads/main/'
+
+
+def _download(fname):
+    if not fname.exists():
+        url = _URL + fname.name
+        print(f'Dwownload file {url}')
+        urllib.request.urlretrieve(url, fname)
+    return fname
+
+
+def test_assay(out=None, testit=True):
+    with _tmpdir(out) as tmp:
+        query = _download(tmp / 'example_queries.fasta')
+        genomes = _download(tmp / 'example_database.fasta')
+        if testit:
+            out = tmp / 'probes.blastn'
+            db = tmp / 'db/db.db'
+            call = f'assay_blast "{genomes}" -q "{query}" -o {out} --db {db}'
+            print(call)
+            os.system(call)
+            os.system(call)
+            call = f'assay_analyze {out}'
+            print(call)
+            os.system(call)
+            call = f'assay_analyze {out} --only-primer -o {tmp / "primer"}'
+            print(call)
+            os.system(call)
+            print()
+            print('Tests run successful.')
+
+
+def main():
+    import argparse
+    p = argparse.ArgumentParser('assay_tests')
+    p.add_argument('out', help='output directory (by default a temporary directory)', nargs='?')
+    p.add_argument('-d', '--download', help='only download test files, do not run tests', action='store_true')
+    args = p.parse_args()
+    if args.download and not args.out:
+        p.error('[out] argument needed for --download option')
+    test_assay(args.out, not args.download)
 
 
 if __name__ == '__main__':
-    test_assay()
+    main()
